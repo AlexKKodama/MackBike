175e29c6e0a3b973bdae9be46d0a6e51dcdbde26
alexkkodama <alex.kazuo.kodama@gmail.com>
2026-05-15 01:33:12 -0300

    Added Spring Boot & Docker Compose

---
+++ app/src/main/java/com/mackbike/app/MackBikeApplication.java
@@ -0,0 +1,13 @@
+package com.mackbike.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MackBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MackBikeApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/mackbike/app/MackBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackbike.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MackBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
